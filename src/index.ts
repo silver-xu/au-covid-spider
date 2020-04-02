e7d5d43b1d00809c7f41d307602278e329453c9e
Silver Xu <me@silverxu.com>
2020-04-02 23:07:32 +1100

    Optimise data download

--- src/index.ts
+++ src/index.ts
@@ -5,39 +5,46 @@ import { default as regions } from './config/regions.json';
 import { fetchStats } from './httpClient/statsClient';
 import { upsertStats, upsertLastRefreshDate } from './services/dynamo';
 
-import Git from 'nodegit';
+import fetch from 'node-fetch';
 import csv from 'csv-parser';
 import fs from 'fs';
 import rmfr from 'rmfr';
 import util from 'util';
+import unzipper from 'unzipper';
+import fstream from 'fstream';
 
-const readdir = util.promisify(fs.exists);
+const exists = util.promisify(fs.exists);
+const dataRepoPath = '/tmp';
+const repoUrl = 'https://github.com/CSSEGISandData/COVID-19/archive/master.zip';
 
-const cloneOptions = {
-  fetchOpts: {
-    callbacks: {
-      certificateCheck: function() {
-        return 0;
-      },
-    },
-  },
+const downloadData = async () => {
+  console.log('[Info]: Downloading John Hopkins Datasets from github');
+  const res = await fetch(repoUrl);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(unzipper.Extract({ path: dataRepoPath }));
+
+    res.body.on('error', err => {
+      reject(err);
+    });
+    res.body.on('finish', function() {
+      console.log('[Info]: John Hopkins Datasets successfully downloaded and unzipped');
+      resolve();
+    });
+  });
 };
 
 export const handler = async () => {
-  if (await readdir('/tmp/dataRepo')) {
-    console.log('[Info]: <dataRepo> directory already exists');
-    console.log('[Info]: Dropping <dataRepo> directory');
-    await rmfr('/tmp/dataRepo');
-    console.log('[Info]: <dataRepo> directory dropped');
+  if (await exists(`${dataRepoPath}/COVID-19-master`)) {
+    console.log('[Info]: Data folder not empty, delete!');
+    await rmfr(`${dataRepoPath}/COVID-19-master`);
+    console.log('[Info]: Previous data folder deleted');
   }
 
-  console.log('[Info]: Cloning John Hopkins Datasets from github');
-  await Git.Clone('https://github.com/CSSEGISandData/COVID-19.git', '/tmp/dataRepo', cloneOptions);
-  console.log('[Info]: John Hopkins Datasets successfully cloned');
+  await downloadData();
 
   const results = [];
 
-  fs.createReadStream('/tmp/dataRepo/csse_covid_19_data/csse_covid_19_daily_reports/01-22-2020.csv')
+  fs.createReadStream(`${dataRepoPath}/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/01-22-2020.csv`)
     .pipe(csv())
     .on('data', data => results.push(data))
     .on('end', () => {
