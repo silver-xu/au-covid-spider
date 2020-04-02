595eaceb421e65c42b287d051e6884b9cb82f812
Silver Xu <me@silverxu.com>
2020-04-02 21:40:37 +1100

    Fixing git in lambda

--- src/index.ts
+++ src/index.ts
@@ -13,21 +13,31 @@ import util from 'util';
 
 const readdir = util.promisify(fs.exists);
 
+const cloneOptions = {
+  fetchOpts: {
+    callbacks: {
+      certificateCheck: function() {
+        return 0;
+      },
+    },
+  },
+};
+
 export const handler = async () => {
-  if (await readdir('dataRepo')) {
-    console.log('[Info]: dataRepo directory already exists');
-    console.log('[Info]: Dropping dataRepo directory');
-    await rmfr('dataRepo');
-    console.log('[Info]: dataRepo directory dropped');
+  if (await readdir('/tmp/dataRepo')) {
+    console.log('[Info]: <dataRepo> directory already exists');
+    console.log('[Info]: Dropping <dataRepo> directory');
+    await rmfr('/tmp/dataRepo');
+    console.log('[Info]: <dataRepo> directory dropped');
   }
 
-  console.log('[Info]: cloning John Hopkins Datasets from github'
-  await Git.Clone('https://github.com/CSSEGISandData/COVID-19.git', 'dataRepo');
-  console.log('[Info]: John Hopkins Datasets cloned'
+  console.log('[Info]: Cloning John Hopkins Datasets from github');
+  await Git.Clone('https://github.com/CSSEGISandData/COVID-19.git', '/tmp/dataRepo', cloneOptions);
+  console.log('[Info]: John Hopkins Datasets successfully cloned');
 
   const results = [];
 
-  fs.createReadStream('./dataRepo/csse_covid_19_data/csse_covid_19_daily_reports/01-22-2020.csv')
+  fs.createReadStream('/tmp/dataRepo/csse_covid_19_data/csse_covid_19_daily_reports/01-22-2020.csv')
     .pipe(csv())
     .on('data', data => results.push(data))
     .on('end', () => {
